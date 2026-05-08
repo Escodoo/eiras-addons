0dbfe95e356a189cf480355c1e140e3af24f1043
Wesley Oliveira <wesleygabrieldeoliveira@gmail.com>
2026-05-08 18:08:14 -0300

    [ADD] eiras_purchase_request_custom: add new module

---
+++ eiras_purchase_request_custom/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2026 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Eiras Purchase Request Custom",
+    "summary": """
+        Eiras Purchase Request Custom""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/eiras-addons",
+    "depends": ["purchase_request"],
+    "data": [
+        "views/purchase_request_view.xml",
+    ],
+}
