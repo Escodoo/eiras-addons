c904890fde6d6ce61ce33593e235fd5b60559919
Wesley Oliveira <wesleygabrieldeoliveira@gmail.com>
2026-06-25 11:29:02 -0300

    [ADD] eiras_account_custom: add new module

---
+++ eiras_account_custom/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2026 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Eiras Account Custom",
+    "summary": """
+        Eiras Account Custom""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/eiras-addons",
+    "depends": ["account"],
+    "data": [
+        "views/account_move_views.xml",
+    ],
+}
